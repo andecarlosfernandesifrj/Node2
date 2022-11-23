3339ebc34021c335f998d2c2c0203775028634c1
Raianeproenca <raiane.sana75@gmail.com>
2022-11-23 10:04:16 -0300

    Exemplo-2/Node

--- servidor.Web.js
+++ servidor.Web.js
@@ -1,8 +1,15 @@
 var http = require ('http');
+var n = require('./primeiromodulo')
+var dia = require('./modulo.Data');
+
+//mudando o servidor 
 http.createServer (function (req, res) {
     res.writeHead(200,{'Content-Type': 'text/html'});
     res.write('<b>Aula de Node.js<b> <br>');
-    res.write('<i>Aluna: Raiane Franco</i> <br>');
+    res.write('<i>Aluna: </i> ');
+    res.write(n.nome()+'<br>');
+    //res.write(Date());
+    res.write(dia.diaDaSemana()+'<br>');
     res.end('Bom dia!');
 
 }).listen(80);
