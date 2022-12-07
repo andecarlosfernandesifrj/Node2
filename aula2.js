bb2af2b8792515b52df4420a7e46fbd2edbdbb77
andecarlosfernandes <carlosfernandes@ifrj.edu.br>
2022-12-07 11:52:54 -0300

    Aula 3

--- aula2.js
+++ aula2.js
@@ -4,7 +4,7 @@ var url = require('url');
 http.createServer(function (req, res){
 res.writeHead(200, {'Content-Type': 'text/html'});
 res.write(req.url);
-var q = url.parse(req.url, true).query;
-var txt = "dia: "+q.dia+" <br> mes: "+q.mes+"<br> ano: "+q.ano;
-res.end(txt);
+//var q = url.parse(req.url, true).query;
+//var txt = "<br>dia: "+q.dia+" <br> mes: "+q.mes+"<br> ano: "+q.ano;
+//res.end(txt);
 }).listen(80);
